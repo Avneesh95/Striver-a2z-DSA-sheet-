e5f9137d49a6da995fc079fe3c0668da4b416126
Avneesh <avneesh952003@gmail.com>
2026-07-09 11:36:41 +0530

    ArmStrong number

---
+++ Armstrong.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+/**
+ * Armstrong
+ */
+public class Armstrong {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(isArmstrong(num));
+    }
+
+// private static boolean isArmstrong(int num) {
+//     int original = num;
+
+//     // Count digits
+//     int temp = num;
+//     int digits = 0;
+//     while (temp > 0) {
+//         digits++;
+//         temp /= 10;
+//     }
+
+//     int sum = 0;
+//     temp = num;
+
+//     while (temp > 0) {
+//         int digit = temp % 10;
+
+//         int power = 1;
+//         for (int i = 0; i < digits; i++) {
+//             power *= digit;
+//         }
+
+//         sum += power;
+//         temp /= 10;
+//     }
+
+//     return sum == original;
+// }
+
+
+
+
+private static boolean isArmstrong(int num) {
+    int original = num;
+
+    // Count digits
+    int digits = String.valueOf(num).length();
+
+    int sum = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        sum += Math.pow(digit, digits);
+        num /= 10;
+    }
+
+    return sum == original;
+}
+
+}
+
+
